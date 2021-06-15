f7d3847dd7465bdcecc0f7e06b915e0ae637136d
Valentin Frappier <valentin.frappier@gmail.com>
2021-06-15 12:13:50 +0200

    Updated comments

--- Shiny app/app.R
+++ Shiny app/app.R
@@ -1,17 +1,19 @@
 #Shiny app pour le projet de stage
-#Objectif : Cr?er un diagramme interactif permettant d'illustrer le meilleur intervalle de confiance
+#Objectif : Cr?er un diagramme interactif permettant d'illustrer le meilleur intervalle de 
+#confiance pour estimer la moyenne ou la variance d'une v.a X suivant une distribution normale,
+#ainsi que les quantiles permettant d'illustrer cet intervalle 
 
 library(shiny)
 library(ggplot2)
 library(tidyverse)
 library(tibble)
+library (shinyjs)
 
-
-# Define UI for app that draws a histogram ----
+# Define UI for the app
 ui <- fluidPage(
 
   # App title ----
-  titlePanel("Best CI"),
+  titlePanel("Illustration of the best CI for a chosen parameter, and associated quantiles"),
 
   # Sidebar layout with input and output definitions ----
   sidebarLayout(
@@ -19,7 +21,7 @@ ui <- fluidPage(
     # Sidebar panel for inputs ----
     sidebarPanel(
 
-      # Input: Slider for the number of observations to generate ----
+      # Input: Slider for the number of observations ----
       sliderInput("n", "Number of observations:",
                   value = 20,
                   min = 5,
@@ -31,7 +33,7 @@ ui <- fluidPage(
                   choices = c("mean"="moy",
                               "variance"="var")),
 
-      # Input: Slider for the number of bins ----
+      # Input: Slider for the value of alpha, probability of commiting a type I error  ----
       sliderInput(inputId = "alpha", label = "Select a value for alpha:",
                   min = 0,
                   max = 10,
@@ -41,18 +43,22 @@ ui <- fluidPage(
                   sep = ",",
                   animate = FALSE
                   ),
-#-------------------------------------------------------------------------------
-#Calcul du meilleur IC possible avec ces inputs
-#-------------------------------------------------------------------------------
-      #Input for the comparison criteria wanted
+      
+      #shinyjs::useShinyjs(),
+      #Input: Buttons for selecting the comparison criteria
       radioButtons("criteria", "Comparison criteria you would like to use for determining the best CI:",
                  c("Centered and Symetrical" = "cenandsym",
                    "Minimal length" = "minl")),
+      
+      #Input: Buttons for selecting the quantity to center on, if the centered and symetrical
+#criteria is selected
+      radioButtons("center", "Quantity you would like to center on: ",
+                   c("Mode" = "mode",
+                     "Mean" = "mean",
+                     "Median"="med")),
 
 
-    br(),
-
-      # Input: Slider for the number of bins ----
+      # Input: Slider for the value of lambda, a coefficient before alpha in [0,1] ----
       sliderInput(inputId = "lambda",
                   label = "Select a value for lambda:",
                   min = 0,
@@ -78,10 +84,12 @@ ui <- fluidPage(
 
   )
 )
-# Define server logic required to draw a histogram ----
+# Define server logic required to draw a the distribution of the pivotal variable, its quantiles,
+# and to determine and display the best CI possible for the estimation of each parameter ----
 server <- function(input, output) {
 
-
+#creating a data frame in order to draw the distribution : variables x = choice between two 
+#sequances of values abscisse of the distribution ; y = choice between two probability laws
   tab <- reactive({
     tibble(
       x = switch(
@@ -97,6 +105,8 @@ server <- function(input, output) {
     )
   })
   
+#creating a data frame to plot the quantiles of each distribution (student and chisquare) for 
+#alpha in ]0,1[ 
   tabq <- reactive({
     tibble(
       x2 = switch(
@@ -107,13 +117,14 @@ server <- function(input, output) {
     )
   })
 
-
-  #
+#Outpout that plots the dsitribution selected, the quantile of order lambda*alpha and 
+#1-alpha(1-lambda)
   output$distPlot <- renderPlot({
-    ggplot(tab(), aes(x = x, y = y)) +
-      geom_line() +
-      labs(x = "X", y = expression(f[X](x))) 
-    
+    ggplot(tab(), aes(x = x, y = y)) +        #draws the selected distribution
+      geom_line() +                           #marquers = line
+      labs(x = "X", y = expression(f[X](x)))  #graphics labels
+
+#plots the two quantiles and the color the area from the tails of the distribution to the quantile
     ggplot(tabq(),aes(x2, y = 0, fill = factor(stat(quantile))), show.legend = FALSE) +
       ggridges::stat_density_ridges(
         geom = "density_ridges_gradient",
@@ -126,61 +137,35 @@ server <- function(input, output) {
         labels = scales::label_parse()(c("P(X <= q[0.90]) == 0.90", "P(X >= q[0.90]) == 0.10"))
       ) +
       labs(x = "", y = "") +
-      # theme(legend.position = none) +
       theme_bw() +
+      #vertical black dashed line at the quantile of order lambda*alpha absciss
       geom_vline(xintercept = switch(input$parameter,
                                      moy = qt(input$lambda*(input$alpha/100),input$n-1),
                                      var = qchisq(input$lambda*(input$alpha/100),input$n-1)), 
                  color = "black", linetype = "dashed") +
-      
+     
+      #changing annotations for the quantile of order lambda*alpha and plot scale options  
       annotate(geom = "label", x = switch(input$parameter,
                                           moy = qt(input$lambda*(input$alpha/100),input$n-1),
                                           var = qchisq(input$lambda*(input$alpha/100),input$n-1)),
                                y = switch(input$parameter,
                                           moy = -0.04,
                                           var =-0.001),
-               label = expression(q[alpha])) +
-      
+               label = expression(q[lambda%*%alpha])) +
+      #vertical black dashed line at the quantile of order 1-alpha(1-lambda) absciss
       geom_vline(xintercept = switch(input$parameter,
                                      moy = qt(1-(input$alpha/100)*(1-input$lambda),input$n-1),
                                      var = qchisq(1-(input$alpha/100)*(1-input$lambda),input$n-1)), 
                  color = "black", linetype = "dashed") +
-      
+      #changing annotations for the quantile of order 1-alpha(1-lambda) and plot scale options
       annotate(geom = "label", x = switch(input$parameter,
                                           moy = qt(1-(input$alpha/100)*(1-input$lambda),input$n-1),
                                           var = qchisq(1-(input$alpha/100)*(1-input$lambda),input$n-1)),
                y = switch(input$parameter,
                           moy = -0.04,
                           var =-0.001),
-               label = expression(q[1-alpha])) +
-      theme(legend.position = "none")
+               label = expression(q[1-alpha%*%(1-lambda)])) +
+      theme(legend.position = "none") #Unshow legend
   })
 }
 shinyApp(ui = ui, server = server)
-
-
-
-
-#?tibble
-#?sliderInput
-# exists("parameter")
-#?geom_line
-# ?stat_density_ridges
-
-#?theme
-
-# #?runApp
-# #runApp("C:\Users\valentin\Documents\Universit?\Stage L3\Repositories\stage-vfrappier\Shiny app")
-# #?switch
-# #?distribution
-# #runExample("01_hello")      # a histogram
-#  runExample("02_text")       # tables and data frames
-# # runExample("03_reactivity") # a reactive expression
-# # runExample("04_mpg")        # global variables
-# #runExample("05_sliders")    # slider bars
-# runExample("06_tabsets")    # tabbed panels
-# #runExample("07_widgets")    # help text and submit buttons
-# #runExample("08_html")       # Shiny app built from HTML
-# #runExample("09_upload")     # file upload wizard
-# #runExample("10_download")   # file download wizard
-#runExample("11_timer")      # an automated timer
